e24b6665ec80f56a05a9a069a0c1c4c1e6876620
Teffisk <brad.mcknight91@gmail.com>
2018-12-04 13:43:48 -0800

    set to real time

--- clock.js
+++ clock.js
@@ -2,15 +2,27 @@ console.log("Javascript is running");
 var secondHand = document.querySelector('#second');
 var minuteHand = document.querySelector('#minute');
 var hourHand = document.querySelector('#hour');
-var secondDegrees = 0;
-var minuteDegrees = 0;
-var hourDegrees = 0;
+var now = new Date();
+var startSeconds = now.getSeconds();
+var startMinutes = now.getMinutes()+(now.getSeconds()/60)
+var startHours = now.getHours()+(now.getSeconds()/3600)
+console.log(startHours, startMinutes, startSeconds)
+
+secondHand.style.transform = "rotate(" + startSeconds + "deg)";
+minuteHand.style.transform = "rotate(" + startMinutes + "deg)";
+hourHand.style.transform = "rotate(" + startHours + "deg)";
+
+var secondDegrees = startSeconds*6;
+var minuteDegrees = startMinutes*6;
+var hourDegrees = startHours*30;
 
 setInterval(function(){
 	console.log("Tick");
 	tickSecond();
 	tickMinute();
 	tickHour();
+	var actualTime = new Date();
+	console.log(actualTime)
 }, 1000);
 
 function tickSecond(){
